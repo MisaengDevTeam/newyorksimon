25cf3d1a532c179ab36a9bacebe0e88e69204e64
Simon Lee <misaeng.dev@gmail.com>
2023-07-27 15:15:17 -0400

    dev: admin page template established

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+const AdminPage = ({}) => {
+  return (
+    <section className='flex w-[100vw] h-[100vh] justify-center items-center'>
+      <div>MISAENG USA ADMIN PAGE SHOULD BE MADE HERE</div>
+    </section>
+  );
+};
+export default AdminPage;
